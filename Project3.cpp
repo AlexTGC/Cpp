c1ccb12db4fca759be734c614bf0394997be7f2e
AlexTGC <145557290+AlexTGC@users.noreply.github.com>
2023-09-20 13:48:04 +0300

    19.09. Лекция 5. Ветвления и циклы
    Домашнее задание

---
+++ Project3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a = 10;
+	int b = 1;
+
+	while (a > 0) {
+		b *= a;
+		a--;
+	}
+
+	cout << "Факториал числа 10 равно " << b << endl;
+
+	return 0;
+
+}
